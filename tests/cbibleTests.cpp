4df621f40eb93207fa76e96ed4f8fbcc9767c4f5
Tim Hawes <tim@selfdidactic.com>
2015-07-25 21:10:53 -0400

    Trying to fix unit tests for Travis CI

--- tests/cbibleTests.cpp
+++ tests/cbibleTests.cpp
@@ -11,8 +11,8 @@
 
 TEST_CASE( "Tests for SwordFuncs", "[SwordFuncs]" ) {
 
-  SwordFuncs *sw = new SwordFuncs("KJV");
   SECTION ( "Testing SwordFuncs" ) {
+    SwordFuncs *sw = new SwordFuncs("KJV");
     // REQUIRE( sw->parseInput(const_cast<char *>("Gen 1:1")) ==
     //         " 1 In the beginning God created the heaven and the earth.");
     REQUIRE( sw->modname() == "KJV" );
@@ -21,6 +21,6 @@ TEST_CASE( "Tests for SwordFuncs", "[SwordFuncs]" ) {
              " 1 In the beginning God created the heaven and the earth.");
     REQUIRE( sw->lookup("Rom 8:28") == " 28 And we know that all things work together for good to them that love God, to them who are the called according to his purpose.");
     REQUIRE( sw->currentRef() == "Romans 8:28" );
+    free(sw);
   }
-  free(sw);
 }
